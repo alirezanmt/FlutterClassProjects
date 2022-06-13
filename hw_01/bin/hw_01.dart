994136052c48c9fa3fae78a3d9a815839d930c8b
alirezanmt <alireza.tehran2020@gmail.com>
2022-06-13 16:27:36 +0430

    HW_01

---
+++ hw_01/bin/hw_01.dart
@@ -0,0 +1,8 @@
+import 'package:hw_01/hw_01.dart' as hw_01;
+
+void main() {
+  List<String> info = ["alireza", "nematollahi"];
+  var identification = ["alireza", "nematollahi"];
+  print(info);
+  print(identification);
+}
